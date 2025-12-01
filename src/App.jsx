5f6d974a48a488359db8004d07fb50af83d68bc1
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-02 00:52:15 +0700

    feat: setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Register from './pages/Register'
-import Login from './pages/Login'
-
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import LandingPage from './pages/LandingPage';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import LayoutAdmin from './components/LayoutAdmin';
 
 
 const router = createBrowserRouter([
-  {path: "/register", element: <Register />},
-  {path: "/login", element: <Login />}
-])
-const App = () => {
-  return (
-    <RouterProvider router={router} />
-  )
-}
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+    ]
+  },
+  {
+    path: "/",
+    element: <LayoutAdmin />,
+    children: [
+      { path: "/settings", element: <Settings/> }
+    ]
+  },
+    { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> }
+]);
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
